b42f4c509a6938b40a1c4fb141c5e344bc5e7679
LAPTOP-548UMTQB\Elias <elie.ekahwaji@x-tends.com>
2017-10-23 13:42:28 +0300

    Adding logs to userDetailsService

--- src/main/java/com/xtends/chat/chatBot/security/UserDetailsServiceImpl.java
+++ src/main/java/com/xtends/chat/chatBot/security/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package com.xtends.chat.chatBot.security;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +12,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	private Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+	
 	public static UserDetails ChatUser = new User("chatbot"
 			                                    , "Xtends2infinit*"
 			                                    , true
@@ -21,6 +25,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		logger.info("loadUserByUsername for username:"+username);
 		
 		if("chatbot".equals(username))
 		{
