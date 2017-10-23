5230547c5b13f64fb4d7a49c04d60631a10bd72a
LAPTOP-548UMTQB\Elias <elie.ekahwaji@x-tends.com>
2017-10-23 14:22:37 +0300

    enable web security

--- src/main/java/com/xtends/chat/chatBot/ChatBotApplication.java
+++ src/main/java/com/xtends/chat/chatBot/ChatBotApplication.java
@@ -2,8 +2,12 @@ package com.xtends.chat.chatBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableWebSecurity
 public class ChatBotApplication {
 
 	public static void main(String[] args) {
